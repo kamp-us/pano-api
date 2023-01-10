4aee7a921a7322940eaafb72fe2a61f6c7bb1578
ilker <ikadgzl@gmail.com>
2023-01-10 15:08:49 +0300

    added Comment model with auto migration extension

--- internal/models/models.go
+++ internal/models/models.go
@@ -15,11 +15,25 @@ type Post struct {
 	UserID string
 }
 
+type Comment struct {
+	ID      uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
+	Content string
+
+	UserID   string
+	PostID   string
+	ParentID string
+}
+
 func AutoMigrate(db *gorm.DB) error {
 	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
+
+	if err := db.AutoMigrate(&Comment{}); err != nil {
+		return err
+	}
 
 	if err := db.AutoMigrate(&Post{}); err != nil {
 		return err
 	}
+
 	return nil
 }
