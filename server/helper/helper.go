f748427f75045e82aab356ce50afff1739450bba
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-01-14 23:58:46 +0300

    Add ConvertToUpvoteModel helper function

--- server/helper/helper.go
+++ server/helper/helper.go
@@ -10,6 +10,10 @@ func ConvertToPostModel(p *models.Post) *api.Post {
 	return &api.Post{Id: p.ID.String(), Title: p.Title, Url: p.Url, Content: p.Content, Slug: p.Slug, UserId: p.UserID}
 }
 
+func ConvertToUpvoteModel(u *models.Upvote) *api.Upvote {
+	return &api.Upvote{Id: u.ID.String(), EntityId: u.EntityID, EntityType: u.EntityType, UserId: u.UserID}
+}
+
 func ConvertToStringPtr(value *wrapperspb.StringValue) *string {
 	val := value.GetValue()
 	return &val
