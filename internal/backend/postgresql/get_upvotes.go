c70175816e64bb37d32d59b661217ceba4122553
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-01-15 03:07:25 +0300

    Add query condition to GetUpvotes

--- internal/backend/postgresql/get_upvotes.go
+++ internal/backend/postgresql/get_upvotes.go
@@ -7,7 +7,7 @@ import (
 
 func (b *PostgreSQLBackend) GetUpvotes(ctx context.Context, entityId string, entityType string) ([]*models.Upvote, error) {
 	var upvotes []*models.Upvote
-	result := b.DB.Find(&upvotes)
+	result := b.DB.Where("entity_id = ? AND entity_type = ?", entityId, entityType).Find(&upvotes)
 	if result == nil {
 		return nil, result.Error
 	}
